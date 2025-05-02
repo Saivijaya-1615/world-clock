84c1ebe6729c15bb869bdfbd230759d73903fb2d
Elli Vijaya Lakshmi <vijayalakshmi76330@gmail.com>
2025-05-02 12:16:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function updateTime() {
+  let laTime = moment().tz("America/Los_Angeles");
+  document.querySelector("#la-date").innerHTML = laTime.format("MMMM Do YYYY");
+  document.querySelector("#la-time").innerHTML = laTime.format("h:mm:ss A");
 
+  let parisTime = moment().tz("Europe/Paris");
+  document.querySelector("#paris-date").innerHTML = parisTime.format("MMMM Do YYYY");
+  document.querySelector("#paris-time").innerHTML = parisTime.format("h:mm:ss A");
+}
+
+setInterval(updateTime, 1000);
